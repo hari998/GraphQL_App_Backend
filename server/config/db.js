4c7baa7c283e6a7aea7379e0b5324c12d32a9071
hari998 <hariomtiwari0241@gmail.com>
2022-06-13 17:31:19 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const connectDB = () => {
     console.log(`connecting to mongo..`)
     mongoose.connect(process.env.MONGO_URI)
         .then((r) => console.log(`mongo-connected`))
-        .catch((error) => handleError(error))
+        .catch((error) => console.log(error))
 }
 
 // // connect using async/await
